9e1a1edcd6337469fc7de4afd19f3a6460593bdc
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-05-02 19:01:38 +0200

    completato

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-let grid = document.getElementById("grid")
+let grigm lia = document.getElementById("grid");
+
+for (let i = 1; i <= 100; i++) {
+	
+	// Con Virtual-DOM e append
+	let elemento = creaQuadrato(i);
+
+	griglia.append(elemento);
+
+}
+
+
+function creaQuadrato(contenuto) {
+
+	//Crea un quadrato in virtual DOM con del contenuto
+	let square = document.createElement("div");
+	square.classList.add("square");
+	
+
+	// Aggiungi un event-listener
+	square.addEventListener("click", function() {
+		console.log("cliccato", this); //in questo caso this = square
+		this.classList.toggle("evidenziata");
+        square.innerText = contenuto;
+	});
+
+	//restituisci il quadrato al chiamante
+	return square;
+}
